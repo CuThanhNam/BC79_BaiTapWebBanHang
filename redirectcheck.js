bf5a29635e636582f9c14b9b563ccf596d709961
CuThanhNam <your@email.com>
2025-01-19 21:45:35 +0700

    day giao dien trang san pham

---
+++ redirectcheck.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-restricted-globals */
+
+(() => {
+  function gup(name, url) {
+    const n = name.replace(/[[]/, "\\[").replace(/[\]]/, "\\]");
+    const u = url || location.href;
+
+    const regexS = `[\\?&]${n}=([^&#]*)`;
+    const regex = new RegExp(regexS);
+    const results = regex.exec(u);
+
+    return results == null ? null : results[1];
+  }
+
+  window.addEventListener("load", () => {
+    if (window.location.search.indexOf("partnerurl=") > -1) {
+      const url = decodeURIComponent(gup("partnerurl", location.href));
+
+      location.href = url;
+
+      return;
+    }
+
+    window.setTimeout(() => {
+      chrome.runtime.sendMessage({ action: "get_tabstatus" }, (response) => {
+        if (response) {
+          chrome.runtime.sendMessage({ action: "close_current_tab" }, () => {});
+        }
+      });
+    }, 3e3);
+  });
+})();
